b39b5190725a3346e28e613b0a984385ae38bd4f
飞的不是很熟练的猪 <1845620049@qq.com>
2024-04-05 00:20:26 +0800

    add slice union diff function

---
+++ slice/MapReduce.go
@@ -0,0 +1,6 @@
+package slice
+
+// type Mapper = func[T, S any](a T) S
+// 方法实现不同不要使用泛型。
+type myMapReduce[T, S any] interface {
+}
--- slice/calculation.go
+++ slice/calculation.go
@@ -1,19 +1,54 @@
 package slice
 
-import "math"
-
 // Intersect 求交集
-func Intersect[T comparable](vars1, vars2 []T) []T {
-	minLen := int(math.Min(float64(len(vars1)), float64(len(vars2))))
-	m := make(map[T]struct{}, minLen/2)
-	res := make([]T, 0, minLen/2)
+func Intersect[T comparable](vars1 []T, other ...[]T) []T {
+	emCap := len(vars1) / 2
+	m := make(map[T]struct{}, emCap)
+	res := make([]T, 0, emCap)
+	for _, t := range vars1 {
+		m[t] = struct{}{}
+	}
+	for _, ts := range other {
+		for _, t := range ts {
+			if _, ok := m[t]; ok {
+				res = append(res, t)
+			}
+		}
+	}
+	return res
+}
+
+// Union 求并集
+func Union[T comparable](vars ...[]T) []T {
+	emCap := len(vars) * 16
+	m := make(map[T]struct{}, emCap)
+	res := make([]T, 0, emCap)
+	for _, ts := range vars {
+		for _, t := range ts {
+			m[t] = struct{}{}
+		}
+	}
+	for t, _ := range m {
+		res = append(res, t)
+	}
+	return res
+}
+
+// Diff 求vars1对vars2的差集
+func Diff[T comparable](vars1, vars2 []T) []T {
+	emCap := len(vars1) * 4 / 5
+	m := make(map[T]struct{}, emCap)
+	res := make([]T, 0, emCap)
 	for _, t := range vars1 {
 		m[t] = struct{}{}
 	}
 	for _, t := range vars2 {
 		if _, ok := m[t]; ok {
-			res = append(res, t)
+			delete(m, t)
 		}
+	}
+	for t, _ := range m {
+		res = append(res, t)
 	}
 	return res
 }
